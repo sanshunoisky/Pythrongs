6a835ee437c90f91bc86d1b61e513449f7b7ba7d
sanshunoisky <sanjaysanjiv1998@gmail.com>
2017-05-08 14:19:44 +0530

    Add files via upload

---
+++ test 1.py
@@ -0,0 +1,21 @@
+list_fuel_types = ['petrol', 'diesel', 'kerosene']
+cheaplist = ['diesel', 'kerosene']
+
+print (list_fuel_types[1 : ])
+print (cheaplist)
+
+cheaplist[1] + 'idk'
+
+listone = list_fuel_types + cheaplist
+
+print(listone)
+
+mata = 'juan mata'
+dicty = {}
+dicty['name'] = mata
+dicty['age'] = 32
+
+print (dicty)
+print (dicty.keys())
+print(dicty.values())
+
